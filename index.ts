d0ad812590b34332aa914d6dbd61fc3e8c3cc985
Nemanja Turanjanin <nemanja@turanjanin.net>
2024-12-03 02:15:04 +0100

    v1.0.5

---
+++ index.ts
@@ -0,0 +1,4 @@
+import 'reflect-metadata';
+export { ContainerResolver } from './src/container/container-resolver';
+export { Provider } from './src/container/dto/provider.dto';
+export { Injectable } from './src/container/decorators/injectable.decorator';
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import 'reflect-metadata';
-export { ContainerResolver } from './container/container-resolver';
-export { Provider } from './container/dto/provider.dto';
-export { Injectable } from './container/decorators/injectable.decorator';
